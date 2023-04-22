6dcd7547154bf874ef2d4fa8e11b48babe805208
asarabias <asarabias@unicesar.edu.co>
2023-04-21 23:53:24 -0500

    Agregando a la interfaz el boton agregar o new

--- front-end/admin/src/components/main/typeVehicle/TypeVehicle.jsx
+++ front-end/admin/src/components/main/typeVehicle/TypeVehicle.jsx
@@ -3,15 +3,16 @@ import DynamicTable from '../../parcials/DynamicTable';
 import { getWithJWT } from '../../../service/methodAPI';
 import Loading from '../../Loading';
 import EmptyAnswer from '../../EmptyAnswer';
+import { Link } from 'react-router-dom';
 
-const TypeVehicle = () => {
+const TypeVehicle = ( {setBreadcrumb }) => {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(false);
     const tableStructure = {
         thead: ["Nombre"],
         tbody: ["name"]
     };
-    const peopleRouter = "/api/typeVehicle";
+    const typeVehicleRouter = "/api/typeVehicle";
 
     useEffect(() => {
         getWithJWTWithParams();
@@ -19,7 +20,7 @@ const TypeVehicle = () => {
 
     const getWithJWTWithParams = () => {
         setLoading(true);
-        getWithJWT(peopleRouter, sessionStorage.getItem('token'))
+        getWithJWT(typeVehicleRouter, sessionStorage.getItem('token'))
             .then(response => {
                 setLoading(false);
                 if (response.status === 200) {
@@ -33,13 +34,14 @@ const TypeVehicle = () => {
 
     return (
         <div className="mx-4 my-3">
+            <div className='text-end me-3'><Link to={"/typeVehicles/new"} onClick={() => setBreadcrumb([{ route: "/typeVehicles", name: "Tipos de Vehiculos" }, { route: "/new", name: "Nuevo" }])}><i className="fa-solid fa-plus ms-3 text-success"></i></Link></div>
             {loading ?
                 <Loading />
                 :
                 data.length === 0 ?
                     <EmptyAnswer />
                     :
-                    <DynamicTable tableStructure={tableStructure} data={data}/>
+                    <DynamicTable tableStructure={tableStructure} data={data} router={"/typeVehicles"} title={"Tipos de Vehiculos"} setBreadcrumb={setBreadcrumb} backRouter={typeVehicleRouter}/>
             }
 
         </div>
