85ac4dd2e85dc4de3fa8d4e62f728078e1f73101
asarabias <asarabias@unicesar.edu.co>
2023-04-21 23:55:06 -0500

    Agregando la opcion de Cerrar sesion

--- front-end/admin/src/components/parcials/TopBar.jsx
+++ front-end/admin/src/components/parcials/TopBar.jsx
@@ -1,7 +1,15 @@
 import { Navbar, Dropdown, DropdownButton } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate} from 'react-router-dom';
 
 const TopBar = ({ handleShowMenu, nameUser }) => {
+    const navigate = useNavigate();
+
+    const onClickSingOff = () => {
+        sessionStorage.removeItem('token');
+        navigate("/");
+        window.location.reload();
+    }
+
     return (
         <Navbar className="bg-success p-0" sticky="top">
             <Navbar.Brand className="ps-4 text-light" onClick={handleShowMenu}><Link to={'/'}><strong className="text-white">OCR-POPULAR</strong></Link></Navbar.Brand>
@@ -16,7 +24,7 @@ const TopBar = ({ handleShowMenu, nameUser }) => {
                         <Dropdown.Item eventKey="2">Perfil</Dropdown.Item>
                         <Dropdown.Item eventKey="3">Configuracion</Dropdown.Item>
                         <Dropdown.Divider />
-                        <Dropdown.Item eventKey="1">Cerrar Sesion</Dropdown.Item>
+                        <Dropdown.Item eventKey="1" onClick={() => onClickSingOff()}>Cerrar Sesion</Dropdown.Item>
                     </DropdownButton>
                 </Navbar.Text>
             </Navbar.Collapse>
