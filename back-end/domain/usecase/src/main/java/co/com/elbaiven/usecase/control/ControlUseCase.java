59c379e91c990fc66ce1c2f943c90c27329bd982
asarabias <asarabias@unicesar.edu.co>
2023-06-24 10:27:40 -0500

    ajustando operadores de reactive

--- back-end/domain/usecase/src/main/java/co/com/elbaiven/usecase/control/ControlUseCase.java
+++ back-end/domain/usecase/src/main/java/co/com/elbaiven/usecase/control/ControlUseCase.java
@@ -2,10 +2,12 @@ package co.com.elbaiven.usecase.control;
 
 import co.com.elbaiven.model.control.Control;
 import co.com.elbaiven.model.control.gateways.ControlRepository;
+import co.com.elbaiven.model.state.State;
 import co.com.elbaiven.model.state.gateways.StateRepository;
 import co.com.elbaiven.model.utils.Constants;
 import co.com.elbaiven.model.utils.GenericDatetimeFormatter;
 import co.com.elbaiven.model.utils.ModelListCompleteWithPages;
+import co.com.elbaiven.model.vehicle.Vehicle;
 import co.com.elbaiven.model.vehicle.gateways.VehicleRepository;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
@@ -19,43 +21,38 @@ public class ControlUseCase {
     private final ControlRepository controlRepository;
     private final VehicleRepository vehicleRepository;
     private final StateRepository stateRepository;
-    private final GenericDatetimeFormatter  genericDatetimeFormatter;
+    private final GenericDatetimeFormatter genericDatetimeFormatter;
 
-    public Mono<Control> create(Control control){
-        return controlRepository.create(control);
-    }
-    public Mono<Control> placaExist(String placa, String state){
-
-        return vehicleRepository.getLicensePlate(placa)
-                .flatMap(vehicle -> stateRepository.getName(state)
-                        .flatMap(rtaState->controlRepository.create(getControl(vehicle.getId(),rtaState.getId()))
-                )
-        );
+    public Mono<Control> create(String placa, String state) {
+        Mono<State> stateMono = stateRepository.getName(state);
+        Mono<Vehicle> vehicleMono = vehicleRepository.getLicensePlate(placa);
+        return Mono.zip(vehicleMono, stateMono)
+                .flatMap(tupla -> controlRepository.create(getControl(tupla.getT1().getId(),tupla.getT2().getId())));
     }
 
-    public Mono<Control> read(Long id){
+    public Mono<Control> read(Long id) {
         return controlRepository.read(id);
     }
 
-    public Mono<Control> update(Long id,Control control){
-        return controlRepository.update(id,control);
+    public Mono<Control> update(Long id, Control control) {
+        return controlRepository.update(id, control);
     }
 
-    public Mono<Void> delete(Long id){
+    public Mono<Void> delete(Long id) {
         return controlRepository.delete(id);
     }
 
-    public Mono<ModelListCompleteWithPages> getAll(Integer page, Integer pageSize, String typeSearch, String search){
+    public Mono<ModelListCompleteWithPages> getAll(Integer page, Integer pageSize, String typeSearch, String search) {
         return controlRepository.getAll(page, pageSize, typeSearch, search)
                 .collectList()
-                .flatMap(e->controlRepository.count()
-                        .map(count->ModelListCompleteWithPages.
-                                getModelListCompleteWithPages(Arrays.asList(e.toArray()),count, page +1, pageSize)
+                .flatMap(e -> getCount(typeSearch, search)
+                        .map(count -> ModelListCompleteWithPages.
+                                getModelListCompleteWithPages(Arrays.asList(e.toArray()), count, page + 1, pageSize)
                         )
                 );
     }
 
-    private Mono<Long> getCount(String typeSearch, String search){
+    private Mono<Long> getCount(String typeSearch, String search) {
         switch (typeSearch) {
             case "date":
                 return controlRepository.countFindByDate(search + '%');
@@ -68,7 +65,7 @@ public class ControlUseCase {
         }
     }
 
-    private Control getControl(Long idPlaca, Long idState){
+    private Control getControl(Long idPlaca, Long idState) {
         genericDatetimeFormatter.setDateTimeFormatter(DateTimeFormatter.ofPattern(Constants.DATE_PATTERN));
         return Control.builder()
                 .date(genericDatetimeFormatter.getDatetime())
