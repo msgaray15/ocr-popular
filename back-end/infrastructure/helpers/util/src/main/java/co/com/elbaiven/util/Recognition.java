58cd713b2166177c069bb7b60ce2ae5a078f08ff
asarabias <asarabias@unicesar.edu.co>
2023-06-24 10:29:39 -0500

    Clase para obtener las credencialesd desde las variables de entorno

--- back-end/infrastructure/helpers/util/src/main/java/co/com/elbaiven/util/Recognition.java
+++ back-end/infrastructure/helpers/util/src/main/java/co/com/elbaiven/util/Recognition.java
@@ -1,4 +1,14 @@
 package co.com.elbaiven.util;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
 public class Recognition {
+    @Value("${recognition.client-id}")
+    private String clientId;
+    @Value("${recognition.client-secret}")
+    private String clientSecret;
 }
