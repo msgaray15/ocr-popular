e6b20dee82770b7ea5f232edc6e27a53e4e6ba2e
asarabias <asarabias@unicesar.edu.co>
2023-08-25 20:56:25 -0500

    Ajuste al formato de la fecha

--- back-end/domain/model/src/main/java/co/com/elbaiven/model/utils/Constants.java
+++ back-end/domain/model/src/main/java/co/com/elbaiven/model/utils/Constants.java
@@ -1,7 +1,7 @@
 package co.com.elbaiven.model.utils;
 
 public class Constants {
-    public static final String DATE_PATTERN = "yyyy/MM/dd HH:mm:ss";
+    public static final String DATE_PATTERN = "yyyy-MM-dd HH:mm:ss";
     public static final String DEFAULT_VALUE_PAGE = "1";
     public static final String DEFAULT_VALUE_PAGE_SIZE = "15";
     public static final String DEFAULT_VALUE_TYPE_SEARCH= "";
