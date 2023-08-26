7123a9a18bdde2deee47b80421f7ca262c33514d
asarabias <asarabias@unicesar.edu.co>
2023-08-25 21:00:12 -0500

    creando vista DailyReport

--- front-end/admin/src/components/main/indexTabs/dailyReport/DailyReport.jsx
+++ front-end/admin/src/components/main/indexTabs/dailyReport/DailyReport.jsx
@@ -1,52 +1,133 @@
-import { Form, Button,Card } from 'react-bootstrap';
+import { Form, Button, Card, Spinner } from 'react-bootstrap';
 import DynamicTable from '../../../parcials/DynamicTable';
+import { getWithJWT } from '../../../../service/methodAPI';
 import { useState, useEffect } from 'react';
+import Loading from '../../../Loading';
+import { defaultPageSize } from '../../../../service/tools';
+import EmptyAnswer from '../../../EmptyAnswer';
+import PaginationTables from '../../../parcials/PaginationTables';
 
 const DailyReport = () => {
-    const [fecha, setFecha] = useState("");
-    const prueba = {
-        columns: ["Fecha", "Placa", "Registro"],
-        rows: [
-            ["fecha 1", " Placa 1", "Regitro 1"],
-            ["fecha 2", " Placa 2", "Regitro 2"],
-            ["fecha 3", " Placa 3", "Regitro 3"]
-        ]
-    }
+    const [data, setData] = useState([]);
+    const [countInput, setCountInput] = useState(0);
+    const [countOut, setCountOut] = useState(0);
+    const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
+    const getDataToday = () => {
         let today = new Date();
         let day = today.getDate();
+        let textDay= day > 10 ? day : "0" + day;
         let month = today.getMonth() + 1;
         let textMonth = month > 10 ? month : "0" + month;
-        console.log("textMonth: ", textMonth);
         let year = today.getFullYear();
-        
-    }, []);
+        return year + "/" + textMonth + "/" + textDay;
+    }
+
+    const [form, setForm] = useState({
+        page: 1,
+        pageSize: defaultPageSize,
+        typeSearch: "date",
+        textSearch: getDataToday()
+    });
+    const tableStructure = {
+        thead: ["Fecha", "Placa", "Usuario", "Registro"],
+        tbody: ["date", ["vehicle", "licensePlate"], ["vehicle", "user", "person", "name"], ["state", "name"]]
+    };
+    const controlRouter = "/api/control";
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        name === "pageSize" ? setForm({ ...form, [name]: value, page: 1 }) : setForm({ ...form, [name]: value });
+    };
+
+    useEffect(() => {
+        getWithJWTWithParams(form.page);
+    }, [form.page, form.pageSize]);
+
+    const buttonSearchDataToday = () => {
+        handleChange({
+            target: {
+                name: "textSearch",
+                value: getDataToday()
+            }
+        });
+    }
+
+    const buttonSearch = () => {
+        getWithJWTWithParams(1);
+        handleChange({
+            target: {
+                name: "page",
+                value: 1
+            }
+        });
+    }
+
+    const getWithJWTWithParams = (formPage) => {
+        setLoading(true);
+        let routerWithParams = controlRouter + "?page=" + formPage + "&pageSize=" + form.pageSize;
+        if (form.textSearch.length > 0) routerWithParams = routerWithParams + "&typeSearch=" + form.typeSearch + "&search=" + form.textSearch;
+        getWithJWT(routerWithParams, sessionStorage.getItem('token'))
+            .then(response => {
+                setLoading(false);
+                if (response.status === 200) {
+                    let data = response.data;
+                    setData(data);
+                    setCountInput(getCountStateFromData(data.list, "input"));
+                    setCountOut(getCountStateFromData(data.list, "out"));
+                } else {
+                    console.log("Error");
+                }
+            })
+            .catch(err => console.log(err));
+    }
+
+    const getCountStateFromData = (data, state) => {
+        return data.filter(item => item.state.name == state).length;
+    }
 
     return (
-        <div className="row m-3 flex-nowrap-content-center">
+        <div className="row m-3 flex-nowrap-content-center justify-content-evenly align-items-start">
             <div className="col-md-4 m-3 p-3 border rounded">
                 <Form>
-                    <Form.Label className="ms-3">Fecha:</Form.Label>
-                    <Form.Group className="ms-4">
-                        <Form.Control type="text" placeholder=" Ejem: AAA000" className="w-auto min-vw-50 d-inline" />
-                        <Button className="ms-3 align-top bg-success"><i className="fa-solid fa-magnifying-glass"></i></Button>
+                    <div className="py-2 d-flex justify-content-between align-items-start">
+                        <Form.Label><h5>Fecha:</h5></Form.Label>
+                        {data?.pages?.totalRecords > 0 ?
+                            <PaginationTables handleChange={handleChange} pages={data?.pages} formPageSize={form.pageSize} />
+                            :
+                            ""
+                        }
+                    </div>
+                    <Form.Group className="ms-4 d-flex">
+                        <div className="border rounded p-1">
+                            <input type="text" name="textSearch" className="inputTypeTextRepresentDate" onChange={handleChange} value={form.textSearch} />
+                            <input type="datetime-local" className="inputTypeDate" />
+                        </div>
+                        <Button className="ms-3 align-top bg-success" onClick={() => buttonSearch()}><i className="fa-solid fa-magnifying-glass"></i></Button>
+                        <Button className="ms-3 align-top bg-success" onClick={() => buttonSearchDataToday()}><i class="fa-solid fa-rotate-left"></i></Button>
                     </Form.Group>
                 </Form>
-                <div className="mt-4">
-                    <h5>Datos Propietario</h5>
-                    <div className="ms-3 mt-3">
-                        <h6 >Nombre:</h6>
+                <div className="mt-3 d-flex justify-content-evenly">
+                    <div className="p-4 text-center">
+                        <h3 className="text-success">{countInput}</h3>
+                        <h5>Entradas</h5>
                     </div>
-                    <div className="ms-3 mt-3">
-                        <h6 >Identificacion:</h6>
+                    <div className="p-4 text-center">
+                        <h3 className="text-success">{countOut}</h3>
+                        <h5>Salidas</h5>
                     </div>
                 </div>
 
             </div>
 
-            <div className="col-md-7 m-3 p-3 border rounded">
-                <DynamicTable tableData={prueba} />
+            <div className="col-md-7 m-3 px-3 border rounded">
+                {loading ?
+                    <Loading />
+                    :
+                    data?.pages?.totalRecords === 0 ?
+                        <EmptyAnswer />
+                        :
+                        <DynamicTable tableStructure={tableStructure} data={data?.list} />
+                }
             </div>
         </div>
     );
