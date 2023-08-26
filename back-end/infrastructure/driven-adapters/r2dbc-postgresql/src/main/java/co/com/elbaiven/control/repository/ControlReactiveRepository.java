974511c0fd035dadd56a44fe2ba53044330e5d23
asarabias <asarabias@unicesar.edu.co>
2023-08-25 20:59:23 -0500

    ajustando querys a la base de datos

--- back-end/infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/elbaiven/control/repository/ControlReactiveRepository.java
+++ back-end/infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/elbaiven/control/repository/ControlReactiveRepository.java
@@ -9,18 +9,24 @@ import reactor.core.publisher.Mono;
 // TODO: This file is just an example, you should delete or modify it
 public interface ControlReactiveRepository extends ReactiveCrudRepository<ControlModel, Long> {
 
-    @Query(value="SELECT * FROM controls c  WHERE c.date LIKE :date LIMIT :pageSize OFFSET :page")
-    Flux<ControlModel> findByDate(String date,Integer pageSize, Integer page);
-    @Query(value="SELECT COUNT(*) FROM controls c  WHERE c.date LIKE :date")
+    @Query(value = "SELECT * FROM controls c  WHERE c.date BETWEEN :dateStart AND :dateEnd LIMIT :pageSize OFFSET :page")
+    Flux<ControlModel> findByDate(String dateStart, String dateEnd, Integer pageSize, Integer page);
+
+    @Query(value = "SELECT COUNT(*) FROM controls c  WHERE c.date LIKE :date")
     Mono<Long> countFindByDate(String date);
-    @Query(value="SELECT * FROM controls LIMIT :pageSize OFFSET :page")
+
+    @Query(value = "SELECT * FROM controls LIMIT :pageSize OFFSET :page")
     Flux<ControlModel> findAll(Integer pageSize, Integer page);
-    @Query(value="SELECT * FROM controls c  WHERE c.id_state = :idState LIMIT :pageSize OFFSET :page")
+
+    @Query(value = "SELECT * FROM controls c  WHERE c.id_state = :idState LIMIT :pageSize OFFSET :page")
     Flux<ControlModel> findByIdState(Long idState, Integer pageSize, Integer page);
-    @Query(value="SELECT COUNT(*) FROM controls c  WHERE c.id_state = :idState")
+
+    @Query(value = "SELECT COUNT(*) FROM controls c  WHERE c.id_state = :idState")
     Mono<Long> countFindByIdState(Long idState);
-    @Query(value="SELECT * FROM controls c  WHERE c.id_vehicle = :idVehicle LIMIT :pageSize OFFSET :page")
+
+    @Query(value = "SELECT * FROM controls c  WHERE c.id_vehicle = :idVehicle LIMIT :pageSize OFFSET :page")
     Flux<ControlModel> findByIdVehicle(Long idVehicle, Integer pageSize, Integer page);
-    @Query(value="SELECT COUNT(*) FROM controls c  WHERE c.id_vehicle = :idVehicle")
+
+    @Query(value = "SELECT COUNT(*) FROM controls c  WHERE c.id_vehicle = :idVehicle")
     Mono<Long> countFindByIdVehicle(Long idVehicle);
 }
