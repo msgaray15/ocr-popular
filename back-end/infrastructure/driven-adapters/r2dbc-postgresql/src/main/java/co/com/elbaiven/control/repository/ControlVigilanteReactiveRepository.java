8a90ad26b5f84f195757b77d4290b0695e57cfa6
asarabias <asarabias@unicesar.edu.co>
2023-12-01 03:39:18 -0500

    Ajustes para dockerizar la aplicacion

---
+++ back-end/infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/elbaiven/control/repository/ControlVigilanteReactiveRepository.java
@@ -0,0 +1,34 @@
+package co.com.elbaiven.control.repository;
+
+import co.com.elbaiven.control.model.ControlModel;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.sql.Timestamp;
+
+// TODO: This file is just an example, you should delete or modify it
+public interface ControlVigilanteReactiveRepository extends ReactiveCrudRepository<ControlModel, Long> {
+
+    @Query(value = "SELECT * FROM controls c  WHERE c.date BETWEEN :dateStart AND :dateEnd LIMIT :pageSize OFFSET :page")
+    Flux<ControlModel> findByDate(Timestamp dateStart, Timestamp dateEnd, Integer pageSize, Integer page);
+
+    @Query(value = "SELECT COUNT(*) FROM controls c  WHERE c.date BETWEEN :dateStart AND :dateEnd")
+    Mono<Long> countFindByDate(Timestamp dateStart, Timestamp dateEnd);
+
+    @Query(value = "SELECT * FROM controls LIMIT :pageSize OFFSET :page")
+    Flux<ControlModel> findAll(Integer pageSize, Integer page);
+
+    @Query(value = "SELECT * FROM controls c  WHERE c.id_state = :idState LIMIT :pageSize OFFSET :page")
+    Flux<ControlModel> findByIdState(Long idState, Integer pageSize, Integer page);
+
+    @Query(value = "SELECT COUNT(*) FROM controls c  WHERE c.id_state = :idState")
+    Mono<Long> countFindByIdState(Long idState);
+
+    @Query(value = "SELECT * FROM controls c  WHERE c.id_vehicle = :idVehicle LIMIT :pageSize OFFSET :page")
+    Flux<ControlModel> findByIdVehicle(Long idVehicle, Integer pageSize, Integer page);
+
+    @Query(value = "SELECT COUNT(*) FROM controls c  WHERE c.id_vehicle = :idVehicle")
+    Mono<Long> countFindByIdVehicle(Long idVehicle);
+}
